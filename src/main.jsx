0216865bf2d912c79466f99ad13284805a7e6cd3
Gaurav Sahu <itsgauravsahu7+github@gmail.com>
2024-04-08 17:03:51 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home.jsx';
+import Skills from './Components/Skills.jsx';
+import Projects from './Components/Projects.jsx';
+import Contact from './Components/Contacts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/Skills",
+    element: <Skills />,
+  },
+  {
+    path: "/Projects",
+    element: <Projects />,
+  },
+  {
+    path: "/Contacts",
+    element: <Contact />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
